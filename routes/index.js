c38eb1721e7680c2a2a54ab8df3b18a9cd3f4c7f
unknown <honghaier2012@163.com>
2014-07-27 08:09:42 +0800

    add cocos2dx-html5 demo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Cocos2d-html5 Hello World test' });
 });
 
 module.exports = router;
